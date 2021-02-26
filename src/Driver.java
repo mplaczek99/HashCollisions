cb87eac1424bc5edd7d963df92882938bcb83300
mplaczek99 <mplaczek99@gmail.com>
2021-02-26 00:13:00 -0600

    End of Day 2
    Populated LinearProbingHash.java and fixed some things in Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,14 +1,15 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Driver {
-    final int tableSize = 191;
-    final int doubleFactor = 181;
+    final int tableSize = 191; // hash table size
+    final int doubleFactor = 181; // factor R to be used in double hashing
 
-    OutputStream fout;
-
-    ArrayList<Integer> data = new ArrayList<>();
+    OutputStream fout; // declare and create an output stream
+    ArrayList<Integer> data = new ArrayList<>(); // all keys from the input file
 
     public Driver(int number) {
         try {
@@ -18,7 +19,7 @@ public class Driver {
         }
     }
 
-    public void testKeyValue(String description, HashInterface<Integer, Integer> hashTable, Integer key, Integer value) throws RuntimeException {
+    public void testKeyValue(String description, HashInterface<Integer, Integer> hashTable, final Integer key, final Integer value) throws RuntimeException {
         final int previousCollisions = hashTable.getCollisions();
 
         hashTable.put(key, value);
@@ -26,25 +27,41 @@ public class Driver {
         final Integer retrievedValue = hashTable.get(key);
         final String retrievedText = retrievedValue != null ? toString() : null; // I do not know if typed correctly
 
+        // fout...
+
         if (retrievedValue == null || !(retrievedValue.equals(value))) {
+            // fout...
             throw new RuntimeException("value mismatch");
         }
     }
 
-    public void testInputKey(Integer key, HashInterface<Integer, Integer> lph) {
-        Integer value = key * 2;
+    public void testInputKey(final Integer key, HashInterface<Integer, Integer> lph) { // Removed other hashing
+        final Integer value = key * 2;
+
         testKeyValue("Linear", lph, key, value);
+        // Removed other hashing
+
+        // fout...
     }
 
-    public void testData(String description) {
+    public void testData(final String description) {
+        // fout...
+
         LinearProbingHash<Integer, Integer> lph = new LinearProbingHash(tableSize);
+        // Removed other hashing
 
         for (Integer key : data) {
             testInputKey(key, lph);
         }
+
+        // fout...
+        // fout...
+        // fout...
+
+        // fout...
     }
 
-    public void readData(String inputFile) throws FileNotFoundException {
+    public void readData(final String inputFile) throws FileNotFoundException {
         // InputStream fin = new FileInputStream(inputFile);
         // Integer key
 
@@ -57,7 +74,7 @@ public class Driver {
         scanner.close();
     }
 
-    public void testFile(String inputFilename, String outputFilename) throws FileNotFoundException {
+    public void testFile(final String inputFilename, final String outputFilename) throws FileNotFoundException {
         System.out.println("Input file: " + inputFilename + ", output file: " + outputFilename);
 
         readData(inputFilename);
--- src/LinearProbingHash.java
+++ src/LinearProbingHash.java
@@ -1,31 +1,95 @@
-public class LinearProbingHash<key, value> implements HashInterface {
-    private class Record {
-        public Integer key;
-        public Integer value;
+import java.util.ArrayList;
 
-        public Record(Integer key1, Integer value1) {
+public class LinearProbingHash<Key, Value> implements HashInterface<Integer, Integer> {
+    ArrayList<Record> table; // vector or array type
+    int collisions;
+
+    private class Record {
+        Integer key;
+        Integer value;
+
+        public Record(final Integer key1, final Integer value1) {
             key = key1;
             value = value1;
         }
     }
 
-    public LinearProbingHash(int initialSize) {
+    public LinearProbingHash(int initialSize) { // constructor
         initialSize = 191;
-        //table = new
+
+        table = new ArrayList<>(initialSize);
+        System.out.println(table);
+        collisions = 0;
     }
 
     @Override
-    public Integer get(Integer key) {
-        return 1;
+    public Integer get(final Integer key) {
+        final int index = lookUp(key);
+
+        if (index > table.size()) {
+            return null;
+        }
+
+        Record p = table.get(index);
+
+        return p.value;
     }
 
     @Override
-    public void put(Integer key, Integer value) {
+    public void put(final Integer key, final Integer value) throws RuntimeException {
+        final int index = lookUp(key);
 
+        if (index > table.size()) {
+            throw new RuntimeException("Table is full");
+        }
+
+        Record p = table.get(index);
+
+        if (p == null) {
+            table.set(index, new Record(key, value));
+        } else {
+            p.value = value;
+        }
     }
 
     @Override
-    public int getCollisions() {
-        return 0;
+    public final int getCollisions() {
+        return collisions;
+    }
+
+    private static int hash(final Integer key) {
+        // scramble the key via bitwise operations
+        // for simplicity, it is assumed that Key is some kind of number
+
+        return (key >> 8) | ((key&0xff)<<16);
+    }
+
+    private final int hashIndex(final Integer key) {
+        final int index = hash(key);
+
+        // Stops here? , table is not a thing
+        System.out.println(index % table.size());
+        return index % table.size();
+    }
+
+    private final int lookUp(Integer key) {
+        final int startIndex = hashIndex(key);
+        int index = startIndex;
+
+        while (true) {
+            final Record p = table.get(index);
+
+            if (p == null || p.key.equals(key)) {
+                return index;
+            }
+
+            collisions ++;
+            index ++;
+            index %= table.size();
+
+            if (index == startIndex) {
+                return table.size() + 1;
+            }
+        }
     }
 }
